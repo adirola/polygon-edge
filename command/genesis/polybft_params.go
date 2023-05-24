91ef6ccc0efcbe68b6db0def3638e29ccb0fc7a5
Igor Crevar <crewce@gmail.com>
2023-05-24 14:33:22 +0200

    old fork changes in order to panic if there is some fork inside genesis which is not registered in code

--- command/genesis/polybft_params.go
+++ command/genesis/polybft_params.go
@@ -151,7 +151,7 @@ func (p *genesisParams) generatePolyBftChainConfig(o command.OutputFormatter) er
 		Name: p.name,
 		Params: &chain.Params{
 			ChainID: int64(p.chainID),
-			Forks:   enabledForks,
+			Forks:   enabledForks.ToForks(),
 			Engine: map[string]interface{}{
 				string(server.PolyBFTConsensus): polyBftConfig,
 			},
